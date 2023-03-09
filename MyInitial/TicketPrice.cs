656b9e7023af7f835efc04af46b810c3f71cc4db
000523274 <Cindy.DENNISON@student.tafesa.edu.au>
2023-03-09 21:40:02 +1030

    commit the back stall ticket changes

--- MyInitial/TicketPrice.cs
+++ MyInitial/TicketPrice.cs
@@ -16,6 +16,7 @@ namespace Ticketing
         const decimal mdecBalcony = 35.5m;
         const decimal mdecGeneral = 28.75m;
         const decimal mdecBox = 62.0m;
+        const decimal mdecBack = 15.0m;
         const decimal mdecDiscount = 5.0m; 
 
         private int Section
@@ -65,7 +66,10 @@ namespace Ticketing
              case 3:
                  mPrice = mdecBox;
                  break;
-         }
+            case 4:
+                mPrice = mdecBack;
+                break;
+            }
          if (discount)
          { mPrice -= mdecDiscount; }
 
